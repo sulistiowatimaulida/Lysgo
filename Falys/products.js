fa490610273d5905ba92c1e51c0b425ef351f9b5
Sulistiowati Maulida <sulistiowati.lm@gmail.com>
2026-04-22 17:36:37 +0700

    Add files via upload

---
+++ Falys/products.js
@@ -0,0 +1,26 @@
+const db_produk = [
+    { 
+        id: "batik01", 
+        nama: "Batik Navy", 
+        harga: 345000, 
+        images: ["img/batik-navy.jpg"], 
+        desc: "Motif batik pada kemeja FaLys Collection tersebut merupakan kombinasi antara motif Parang yang legendaris dengan sentuhan ornamen floral/lung-lungan modern. Perpaduan ini menciptakan kesan yang gagah namun tetap luwes.",
+        pemilik: "Design : Sulis"
+    },
+    { 
+        id: "tailor", 
+        nama: "Tailor", 
+        harga: 345000, 
+        images: ["img/batik-bergundy.png"], 
+        desc: "Ingin baju yang nggak pasaran dan pas di badan? Serahkan pada ahlinya di [Nama Brand].",
+        pemilik: "Design : Sulis"
+    },
+    { 
+        id: "tailor", 
+        nama: "Tailor", 
+        harga: 100000, 
+        images: ["img/tailor.jpg"], 
+        desc: "Ingin baju yang nggak pasaran dan pas di badan? Serahkan pada ahlinya di [Nama Brand].",
+        pemilik: "Design : Sulis"
+    }
+];
